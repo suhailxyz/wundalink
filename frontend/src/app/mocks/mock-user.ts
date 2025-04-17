0b46eeec38d9593ad411e336d614511dd0c0f84e
suhailxyz <suhailj17@gmail.com>
2025-04-17 17:25:31 -0400

    chore: add mock link, capitalization

--- frontend/src/app/mocks/mock-user.ts
+++ frontend/src/app/mocks/mock-user.ts
@@ -5,6 +5,7 @@ export const MOCK_USER: MockUser = {
     title: '',  // Empty by default, will show username if not set
     links: [
       { label: 'My Website', url: 'https://example.com', isEditing: false },
+      { label: 'My Social', url: 'https://touchgrass.com', isEditing: false },
       { label: 'GitHub', url: 'https://github.com/myprofile', isEditing: false }
     ]
   };
